0a49301a6cec6e26e7ae7eb872173e1a704173ff
Govindarajan M <govind-19364@govind-19364.csez.zohocorpin.com>
2026-04-23 17:21:07 +0530

    Fix build.sh docker context path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Building Docker image..."
-docker build -t react-devops-app .
+docker build -t react-devops-app -f $(pwd)/Dockerfile $(pwd)
 docker tag react-devops-app govindcloud01/dev:latest
 echo "Build complete!"
