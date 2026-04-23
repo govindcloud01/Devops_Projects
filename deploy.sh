a78a08e78234e0ce312175863e7ec54b3a003dbf
Govindarajan M <govind-19364@govind-19364.csez.zohocorpin.com>
2026-04-23 17:04:52 +0530

    Add all project files

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker pull govindcloud01/dev:latest
 docker stop react-app 2>/dev/null || true
 docker rm react-app 2>/dev/null || true
 docker run -d --name react-app -p 80:80 govindcloud01/dev:latest
-echo "Deployment complete!"
+echo "Deployment complete! "
