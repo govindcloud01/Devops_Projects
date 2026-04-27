42a884470d9651d327889831c983e2a695fc215c
Govindarajan M <govind-19364@govind-19364.csez.zohocorpin.com>
2026-04-27 06:27:06 +0530

    Test

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ echo "Building Docker image..."
 sudo docker build -t react-devops-app -f /home/ubuntu/.jenkins/workspace/react-devops-pipeline/Dockerfile /home/ubuntu/.jenkins/workspace/react-devops-pipeline
 #docker build -t react-devops-app -f $(pwd)/Dockerfile $(pwd)
 docker tag react-devops-app govindcloud01/dev:latest
-echo "Build complete! Fhaaah....!!"
+echo "Build complete! Fhaaah....!!!"
